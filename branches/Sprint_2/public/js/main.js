0f5279672b58ae699f1e89eb0e689462948ed95c
vitAndreA <vitAndreA@4a3c457b-8a84-4006-9402-4c076ffcbad6>
2020-06-05 08:51:33 +0000

    Modifica della modalità di interazione dell'utente con le ratingStars. FIX #53605 #53606 #53607

--- branches/Sprint_2/public/js/main.js
+++ branches/Sprint_2/public/js/main.js
@@ -21,18 +21,30 @@ function clickAnimationStar(element,array){
         document.getElementById(element).addEventListener("click", function(){
 
             var found = false;
+            var unchecking = false;
             array.forEach(function(element1){
-                if (found==false){
+                if (element == element1){
+
+                    //controllo se quello che ho premuto era gia stato premuto
+                    if(document.getElementById(element1+"Input").checked == true){
+
+                        array.forEach(function(element2){
+                            document.getElementById(element2).style["color"] = "black";
+                            document.getElementById(element2+"Input").checked = false;
+                        });
+                        unchecking = true;
+
+                    }else{
+                        document.getElementById(element1).style["color"] = "orange";
+                        document.getElementById(element1+"Input").checked = true;
+                        found = true;
+                    }
+                }else if(found==false && unchecking==false){
                     document.getElementById(element1).style["color"] = "orange";
                     document.getElementById(element1+"Input").checked = false;
                 }else{
                     document.getElementById(element1).style["color"] = "black";
                     document.getElementById(element1+"Input").checked = false;
-                }
-                if (element == element1){
-                    document.getElementById(element1).style["color"] = "orange";
-                    document.getElementById(element1+"Input").checked = true;
-                    found = true;
                 }
             });
 
